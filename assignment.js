359aa94fb9ec111d31d0b6ab8a260355c2f37cbc
webzahidul <webzahidul@gmail.com>
2020-08-08 09:34:19 +0600

    second-commit

---
+++ assignment.js
@@ -0,0 +1,17 @@
+
+function feetToMile(feet){
+    var mile = feet * 0.000189394;
+    return mile;
+}
+
+var dhakaToKhulna = feetToMile(500);
+console.log(dhakaToKhulna);
+
+var dhakaToCtg = feetToMile(800);
+console.log(dhakaToCtg);
+
+var dhakaToSylhet = feetToMile(600);
+console.log(dhakaToSylhet);
+
+
+// -----------------------------------
